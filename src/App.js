21f338cc17c67017fa3d73f713a76e261d409432
Nicolas Bugdaci <nicolasbugdaci@mbp-nicolas.lan>
2020-02-09 22:32:51 +0100

    Exercice finalisé

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Section from "./components/Section";
+import data from "./assets/data.json";
+import Logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="logo" src={Logo} />
+      <div className="wrapper">
+        {data.map((elem, index) => {
+          const name = elem.category;
+          const movies = elem.images;
+          return <Section category={name} images={movies} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
